202ef028c6fa4e0774058e528812e3962314f10f
Vedangmodi <modivedang.mv94@gmail.com>
2025-05-21 02:47:53 +0530

    Update firebase.example.js

--- src/firebase.example.js
+++ src/firebase.example.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDvphC5rKNO1NwyQ64BFL_Y6-2rCpKkaOk",
-  authDomain: "chatapp-71ab2.firebaseapp.com",
-  projectId: "chatapp-71ab2",
-  storageBucket: "chatapp-71ab2.firebasestorage.app",
-  messagingSenderId: "225965397214",
-  appId: "1:225965397214:web:9ddaaffc0c0032d0231e4f",
-  measurementId: "G-72PMZV24BG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
